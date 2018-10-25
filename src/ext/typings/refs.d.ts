786757a50c30c9d8ae201685df57f42289b144e0
Sungwoo <sungwoo.cho.dev@gmail.com>
2018-10-25 19:10:10 +0900

    Adjust monaco Definition Path

--- src/ext/typings/refs.d.ts
+++ src/ext/typings/refs.d.ts
@@ -2,4 +2,4 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-/// <reference path='../../node_modules/monaco-editor-core/monaco.d.ts'/>
+/// <reference path='../../../node_modules/monaco-editor/monaco.d.ts'/>
